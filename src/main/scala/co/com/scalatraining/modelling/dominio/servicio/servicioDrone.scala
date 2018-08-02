643880ad9e054ba2811fd106225f7b05284ce16a
DairoArley <dagavergara@gmail.com>
2018-08-01 23:53:13 -0500

    Try en de movimientos del dron

--- src/main/scala/co/com/scalatraining/modelling/dominio/servicio/servicioDrone.scala
+++ src/main/scala/co/com/scalatraining/modelling/dominio/servicio/servicioDrone.scala
@@ -1,17 +1,16 @@
 package co.com.scalatraining.modelling.dominio.servicio
 
 import co.com.scalatraining.modelling.dominio.entidades._
-import com.sun.net.httpserver.Authenticator.Success
 
 import scala.concurrent.Future
-import scala.util.{Success, Try}
+import scala.util.{Failure, Success, Try}
 
 sealed trait AlgebraServicioDrone {
-  def moverDrone(instruccion: Instruccion, drone: Drone): Try[Drone]
-  def girarIzquierda(drone: Drone): Ubicacion
-  def girarDerecha(drone: Drone): Ubicacion
-  def seguirAdelante(drone: Try[ Drone ]): Ubicacion
-  def obstaculoEnMetros(x:Int, y:Int):Try[Drone]
+  def moverDrone(instruccion: Instruccion, drone: Drone): Drone
+  def girarIzquierda(drone: Try [ Drone ]): Try[Ubicacion]
+  def girarDerecha(drone: Try [ Drone ]): Try[Ubicacion]
+  def seguirAdelante(drone: Try[ Drone ]): Try[Ubicacion]
+  def pocisionEsValida(x:Int, y:Int):Boolean
   def llevarAlmuerzo(entrega: Entrega, drone: Drone):Drone
   def llevarPedido(pedido: Pedido, drone: Drone):List[Reporte]
   def tomarPedido(list: List[String]):Try[Pedido]
@@ -19,59 +18,59 @@ sealed trait AlgebraServicioDrone {
 }
 sealed trait InterpretacionServicioDrone extends AlgebraServicioDrone {
 
-  override def moverDrone( instruccion: Instruccion, drone: Drone ): Try[Drone] = {
-
+  override def moverDrone( instruccion: Instruccion, drone: Drone ): Drone = {
     val ubicacion = instruccion match {
       case A() => seguirAdelante(Try(drone) )
-      case I() => girarIzquierda( drone)
-      case D() => girarDerecha( drone)
+      case I() => girarIzquierda( Try (drone) )
+      case D() => girarDerecha( Try (drone) )
     }
-
     //A la redonda
     ubicacion match {
-      case Success(nam) : Try(Drone() )
+      case Success(nam) => Drone(ubicacion.get, drone.capacidad, drone.id)
+      case _ => Drone(Ubicacion(Coordenada(0, 0), Norte('N')), drone.capacidad, drone.id)
     }
-
-    //Drone(ubicacion, drone.capacidad,drone.id)
-    val rango = Try{ Drone(ubicacion, drone.capacidad,drone.id) }
-
-
   }
 
-  override def girarIzquierda(drone: Drone): Ubicacion = {
-    val coordenada = drone.ubicacion.coordenada
-    val retorno = drone.ubicacion.orientacion match {
-      case Norte('N') => Ubicacion(coordenada, Oeste('O') )
-      case Sur('S') => Ubicacion(coordenada, Este('E'))
-      case Oeste('S') => Ubicacion(coordenada, Sur('S'))
-      case Este('E') => Ubicacion(coordenada, Norte('N') )
+  override def girarIzquierda(drone:Try[ Drone ]): Try[Ubicacion] = {
+    val coordenada = drone.get.ubicacion.coordenada
+    val retorno = drone.get.ubicacion.orientacion match {
+      case Norte('N') => Try {Ubicacion(coordenada, Oeste('O') )}
+      case Sur('S') => Try {Ubicacion(coordenada, Este('E'))}
+      case Oeste('S') => Try{ Ubicacion(coordenada, Sur('S')) }
+      case Este('E') => Try{ Ubicacion(coordenada, Norte('N') ) }
     }
     retorno
   }
 
-  override def girarDerecha(drone: Drone): Ubicacion = {
-    val coordenada = drone.ubicacion.coordenada
-    val retorno = drone.ubicacion.orientacion match {
-      case Norte('N') => Ubicacion(coordenada, Este('E'))
-      case Sur('S') => Ubicacion(coordenada, Oeste('O'))
-      case Oeste('O') => Ubicacion(coordenada, Norte('N'))
-      case Este('E') => Ubicacion(coordenada, Sur('S'))
+  override def girarDerecha(drone: Try[Drone]): Try[Ubicacion ]= {
+    val coordenada = drone.get.ubicacion.coordenada
+    val retorno = drone.get.ubicacion.orientacion match {
+      case Norte('N') => Try{ Ubicacion(coordenada, Este('E'))}
+      case Sur('S') => Try{ Ubicacion(coordenada, Oeste('O')) }
+      case Oeste('O') => Try{ Ubicacion(coordenada, Norte('N')) }
+      case Este('E') => Try{ Ubicacion(coordenada, Sur('S')) }
     }
     retorno
   }
 
-  override def seguirAdelante(drone:Try[ Drone ]): Ubicacion = {
+  override def seguirAdelante(drone:Try[ Drone ]): Try[Ubicacion] = {
     val coordenada = drone.get.ubicacion.coordenada
     val orientacion = drone.get.ubicacion.orientacion
-    val retorno = drone.get.ubicacion.orientacion match {
-      case Norte(char) => if( )else
-
-        Ubicacion(Coordenada(coordenada.x, coordenada.y+1), orientacion)
-      case Sur(char) => Ubicacion(Coordenada(coordenada.x, coordenada.y-1), orientacion)
-      case Oeste(char) => Ubicacion(Coordenada(coordenada.x-1, coordenada.y), orientacion)
-      case Este(char) => Ubicacion(Coordenada(coordenada.x+1, coordenada.y), orientacion)
+    drone.get.ubicacion.orientacion match {
+      case Norte(char) =>  if( !pocisionEsValida(coordenada.x, coordenada.y+1) ) Try(throw new Exception("Paila"))else
+        Try{ Ubicacion(Coordenada(coordenada.x, coordenada.y+1), orientacion) }
+      case Sur(char) => if( !pocisionEsValida(coordenada.x, coordenada.y-1) ) Try(throw new Exception("Paila"))else
+        Try{ Ubicacion(Coordenada(coordenada.x, coordenada.y-1), orientacion) }
+      case Oeste(char) => if( !pocisionEsValida(coordenada.x-1, coordenada.y) ) Try(throw new Exception("Paila"))else
+        Try{ Ubicacion(Coordenada(coordenada.x-1, coordenada.y), orientacion) }
+      case Este(char) => if( !pocisionEsValida(coordenada.x+1, coordenada.y) ) Try(throw new Exception("Paila"))else
+        Try{ Ubicacion(Coordenada(coordenada.x+1, coordenada.y), orientacion)}
     }
-    retorno
+  }
+
+//---------- Regla de negocio
+  override def pocisionEsValida(x: Int, y: Int):Boolean = {
+    Math.sqrt(  Math.pow(x,2) + Math.pow(y,2) ) <= 10
   }
 
   override def llevarAlmuerzo(entrega: Entrega, drone: Drone): Drone = {
